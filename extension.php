5893629c64c5325fbb7e2262e13f6163358b3907
win-100 <44981523+win-100@users.noreply.github.com>
2025-08-22 09:25:37 +0200

    feat: replace summary text with icon

--- extension.php
+++ extension.php
@@ -29,7 +29,7 @@ class ArticleSummaryExtension extends Minz_Extension
 
     $entry->_content(
       '<div class="oai-summary-wrap">'
-      . '<button data-request="' . $url_summary . '" class="oai-summary-btn"></button>'
+      . '<button data-request="' . $url_summary . '" class="oai-summary-btn btn btn-small" aria-label="Résumer" title="Résumer"><span class="icon icon-ellipsis"></span></button>'
       . '<div class="oai-summary-loader"></div>'
       . '<div class="oai-summary-log"></div>'
       . '<div class="oai-summary-content"></div>'
