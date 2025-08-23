f8d667939da0b06422434eb7c47a57b79219aaae
win-100 <44981523+win-100@users.noreply.github.com>
2025-08-23 23:05:24 +0200

    feat: add text-to-speech button

--- extension.php
+++ extension.php
@@ -36,10 +36,12 @@ class ArticleSummaryExtension extends Minz_Extension
     ));
     $has_more = trim((string)FreshRSS_Context::$user_conf->oai_prompt_2) !== '';
 
+    $icon_tts = str_replace('<svg ', '<svg class="oai-tts-icon" ', file_get_contents(__DIR__ . '/static/img/play.svg'));
     $icon = str_replace('<svg ', '<svg class="oai-summary-icon" ', file_get_contents(__DIR__ . '/static/img/summary.svg'));
 
     $entry->_content(
       '<div class="oai-summary-wrap">'
+      . '<button class="oai-tts-btn btn btn-small" aria-label="Lire" title="Lire">' . $icon_tts . '</button>'
       . '<button data-request="' . $url_summary . '" class="oai-summary-btn btn btn-small" aria-label="Résumer" title="Résumer">'
       . $icon . '</button>'
       . '<div class="oai-summary-box">'
